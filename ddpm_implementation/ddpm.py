cf557edc067f0cdbbaa5b3661710955cfd673968
vanilladucky <kimhyunbin106@gmail.com>
2025-05-27 16:39:12 +0800

    fix

--- ddpm_implementation/ddpm.py
+++ ddpm_implementation/ddpm.py
@@ -234,8 +234,8 @@ def train_and_eval(epochs, cuda_device=0, image_size = 128, steps=1000, batch_si
         transforms.CenterCrop(image_size),
         transforms.ToTensor(),
         transforms.Normalize(
-            (0.5, 0.5),
-            (0.5, 0.5)
+            (0.5, 0.5, 0.5),
+            (0.5, 0.5, 0.5)
         )
     ])
 
