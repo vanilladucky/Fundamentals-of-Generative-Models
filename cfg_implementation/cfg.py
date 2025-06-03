710b895ca41b771f7d4c1d88fa02f6ff770dd4db
vanilladucky <kimhyunbin106@gmail.com>
2025-06-03 13:07:35 +0800

    fix

--- cfg_implementation/cfg.py
+++ cfg_implementation/cfg.py
@@ -311,7 +311,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--cuda', type=int, default=0, help='CUDA device number (e.g., 0, 1, etc.)')
     parser.add_argument('--epochs', type=int, default=10, help='Number of training epochs')
-    parser.add_argument('--image_size', type=int, default=128, help='Dimension of image')
+    parser.add_argument('--image_size', type=int, default=32, help='Dimension of image')
     parser.add_argument('--steps', type=int, default=1000, help='Number of time steps')
     parser.add_argument('--batch_size', type=int, default=64, help='Batch Size')
     parser.add_argument('--base_lr', type=float, default=2e-4, help='Initial Learning Rate')
