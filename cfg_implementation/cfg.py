3212c947b6d958af514b39b78cf29db6648af4a7
vanilladucky <kimhyunbin106@gmail.com>
2025-06-03 10:36:36 +0800

    f

--- cfg_implementation/cfg.py
+++ cfg_implementation/cfg.py
@@ -129,7 +129,7 @@ def train_and_eval(img_size, batch_size, device, timesteps, epochs = 100, base_l
     )
 
     net = UNet(n_classes=10).to(device)
-    print(summary(net, input_data = (1, 3, img_size, img_size)))
+    print(summary(net, (1, 3, img_size, img_size)))
     cfg = CFG(net = net, img_size=img_size, batch_size=batch_size, device=device, timesteps=timesteps)
     optim = torch.optim.Adam(net.parameters(), lr=base_lr)
     scheduler = SimpleDDPMScheduler(timesteps=timesteps, device=device).to(device)
