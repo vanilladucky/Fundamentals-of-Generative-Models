c13a059726ba36068e1d2c88d991a93e659c706d
vanilladucky <kimhyunbin106@gmail.com>
2025-06-13 10:04:04 +0800

    fix

--- latent_diffusion/training.py
+++ latent_diffusion/training.py
@@ -75,11 +75,11 @@ def train_vae(args):
     
     transform = transforms.Compose([
         transforms.Resize((args.image_size, args.image_size)),
+        transforms.ToTensor(),
         transforms.Normalize(
             mean=[0.485, 0.456, 0.406],
             std =[0.229, 0.224, 0.225]
-            ),
-        transforms.ToTensor(),
+            )
     ])
     dataset = datasets.CIFAR10(root='./data', train=True, transform=transform, download=True)
     loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True)
